aa2667ba9faf351d8f77464d1ba2622ae448f009
Arpeet Kale <arpeetkale@Arpeets-MBP.attlocal.net>
2021-10-16 22:59:13 -0700

    One more test

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -7,7 +7,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to Skiff!</h2>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
