3ceacfe5286f35f663a271e71c6a702b1938b2a0
Arpeet Kale <arpeetkale@Arpeets-MBP.attlocal.net>
2021-10-16 23:36:47 -0700

    VG 2.1

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to Skiff!</h2>
-        <h3>VG's version 1.0</h3>
-        <h4>AK supports</h4>
+        <h3>VG's version 2.1</h3>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
