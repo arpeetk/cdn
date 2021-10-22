5eb2773432fe93c22c077131bc3464f392ca640d
arpeetk <92598539+arpeetk@users.noreply.github.com>
2021-10-21 19:24:56 -0700

    Update App.tsx

--- frontend-app/src/App.tsx
+++ frontend-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>Welcome to Skiff!</h2>
-        <h3>AK's version 3.5</h3>
+        <h3>AK's version 3.0</h3>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
